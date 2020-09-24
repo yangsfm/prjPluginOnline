abf5ed786b28e7c4e7641a3156d2a53d805c0a0c
yangsfm <yangferreira.etec@gmail.com>
2020-09-24 10:25:06 +0900

    Mapa dando erro

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,2 +1,48 @@
-// This is a JavaScript file
+$(document).on("click", "#alerta", function() {
+  navigator.notification.alert("Minha mensagem", null, "Aviso!", "Aceito");
+});
 
+$(document).on("click", "#confirmar", function() {
+  function confirma(buttonIndex) {
+    if(buttonIndex == 1) {
+      navigator.notification.alert("Escolheu a opção B");
+    }else{
+      navigator.notification.alert("Escolheu a opção A");     
+    }
+  }
+  navigator.notification.confirm("A ou B", confirma, "Escolha: ", ['B', 'A']);
+});
+
+$(document).on("click", "#notificar", function() {
+  navigator.notification.beep(1);
+});
+
+$(document).on("click", "#vibrar", function() {
+  navigator.vibrate(1000);
+});
+
+function mostraMapa(lat, long) {
+  L.mapquest.key = 'ybliJHRjLaNpWqLLNi4n4sKAoDCSVflu';
+
+  var map = L.mapquest.map('map', {
+    center: [lat, long],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12
+  });
+
+  map.addControl(L.mapquest.control());
+
+}
+
+$(document).on("click", "#local", function() {
+  var onSuccess = function(position) {
+    mostraMapa(position.coords.latitude, position.coords.longitude)
+  };
+
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+});
